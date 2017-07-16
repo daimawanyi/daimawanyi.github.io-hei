7927b55e0d7b823fc03425f665b9a2206a29b55a
shenmewanyi <2211574160@qq.com>
2017-07-16 22:18:59 +0800

    1
    1

--- js/index.js
+++ js/index.js
@@ -16,11 +16,11 @@ var c =document.documentElement.clientHeight
 
 bg.style.height = H + 'px';
 fff[0].style.opacity = 1;
-content1.style = 'display: block;position:absolute;z-index:1;width:100%;top:' + H + 'px';
-//alert('b'+content1.style.top);
+//content1.style = 'display: block;position:absolute;z-index:1;width:100%;top:' + H + 'px';
+
 $(content1).css('top',100+'px');
-alert('c'+$(content1).css('top'));
-var a=document.body.scrollHeight;
+alert(bg.style.height)
+alert('d'+$(content1).css('top'));
 console.log(H);
 console.log(c);
 
